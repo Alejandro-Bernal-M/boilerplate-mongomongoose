4e2dd83d07b883ecadb54ae143bc7132f25b0e0e
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-03 11:35:58 -0500

    Add delete many

--- myApp.js
+++ myApp.js
@@ -96,8 +96,11 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
+  Person.deleteMany({name: nameToRemove}, function(err, data){
+    if(err) return console.log(err)
+    done(null, data);
+  })
 
-  done(null /*, data*/);
 };
 
 const queryChain = (done) => {

983febd9c926105d69df943df9a82a40608b2713
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-03 11:05:56 -0500

    Add find by food

--- myApp.js
+++ myApp.js
@@ -49,8 +49,12 @@ const findPeopleByName = (personName, done) => {
   })
 };
 
+const food = 'wine'
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, function(err, data){
+    if(err) return console.error(err);
+    done(null , data);
+  })
 };
 
 const findPersonById = (personId, done) => {
